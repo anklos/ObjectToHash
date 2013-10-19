aa2854ab61561f36d83f5ada8a6d1d1b979f84e5
Shane <niusheng@gmail.com>
2013-10-20 08:50:39 +1100

    fix indent

--- base.rb
+++ base.rb
@@ -5,15 +5,17 @@ class Base
   #recursion to convert any level of nested objects to hash
   def to_hash
     h = Hash.new
+    
     instance_variables.each do |attribute|    
-    attribute = attribute.to_s.delete('@')
-    instance = send(attribute)
-    unless instance.instance_variables.empty?
-      h[attribute] = instance.to_hash
-    else
-      h[attribute] = instance
+      attribute = attribute.to_s.delete('@')
+      instance = send(attribute)
+      unless instance.instance_variables.empty?
+        h[attribute] = instance.to_hash
+      else
+        h[attribute] = instance
       end
     end
+    
     h
   end
 end
