2b6611148a6445b15a999b24e0d561e332b0fdae
Shane Niu <shane.niu@rea-group.com>
2013-10-20 14:56:00 +1100

    use to_hash from module. only need to include it in the class where needs to call to_hash

---
+++ object_to_hash_module.rb
@@ -0,0 +1,67 @@
+module ObjectToHash
+
+  #recursion to convert any level of nested objects to hash
+  def to_hash
+    h = Hash.new
+
+    instance_variables.each do |attribute|
+      attribute = attribute.to_s.delete('@')
+      instance = send(attribute)
+      unless instance.instance_variables.empty?
+        klass = instance.class
+
+        unless klass.respond_to? :to_hash
+          klass.send(:include, ::ObjectToHash)
+        end
+
+        h[attribute] = instance.to_hash
+      else
+        h[attribute] = instance
+      end
+    end
+
+    h
+  end
+end
+
+class Account
+  attr_accessor :num, :name
+end
+
+class RatePlan
+  attr_accessor :plan_b, :plan_c
+end
+
+class PlanB
+  attr_accessor :testlala
+end
+
+class Testlala
+  attr_accessor :omg
+end
+
+class RequestData
+  include ObjectToHash
+  attr_accessor :account, :rate_plan
+end
+
+account = Account.new
+account.num = '1111'
+account.name = 'shane'
+
+testlala = Testlala.new
+testlala.omg = 'oooo'
+plan_b = PlanB.new
+plan_b.testlala = testlala
+
+rate_plan = RatePlan.new
+rate_plan.plan_b = plan_b
+rate_plan.plan_c = 'ff'
+
+request_data = RequestData.new
+request_data.account = account
+request_data.rate_plan = rate_plan
+
+hash = request_data.to_hash
+puts hash.inspect
+
